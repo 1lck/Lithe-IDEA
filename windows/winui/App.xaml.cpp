164eae9b56f6c9e6bed554cdf7de739426bf9936
lick <2188718831@qq.com>
2026-08-12 11:48:31 +0800

    chore(winui): log startup XAML failures

--- windows/winui/App.xaml.cpp
+++ windows/winui/App.xaml.cpp
@@ -7,15 +7,23 @@ namespace winrt::Lithe::implementation {
 App::App() {
     InitializeComponent();
 
-#if defined(_DEBUG) && !defined(DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION)
     UnhandledException([](IInspectable const&, Microsoft::UI::Xaml::UnhandledExceptionEventArgs const& event) {
-        if (IsDebuggerPresent()) {
+        const auto path = std::filesystem::temp_directory_path() / L"lithe-winui-error.log";
+        if (const auto file = CreateFileW(path.c_str(), GENERIC_WRITE, FILE_SHARE_READ, nullptr,
+                                          CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+            file != INVALID_HANDLE_VALUE) {
             const auto message = event.Message();
-            static_cast<void>(message);
+            const auto bytes = static_cast<DWORD>(message.size() * sizeof(wchar_t));
+            DWORD written = 0;
+            WriteFile(file, message.c_str(), bytes, &written, nullptr);
+            CloseHandle(file);
+        }
+#if defined(_DEBUG) && !defined(DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION)
+        if (IsDebuggerPresent()) {
             __debugbreak();
         }
-    });
 #endif
+    });
 }
 
 void App::OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&) {
