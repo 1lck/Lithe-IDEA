7daf4173e2ade42079bb1f3707568a8b98435421
lick <2188718831@qq.com>
2026-08-12 11:59:13 +0800

    chore(winui): trace launch stages

--- windows/winui/App.xaml.cpp
+++ windows/winui/App.xaml.cpp
@@ -9,11 +9,14 @@ namespace {
 void WriteStartupError(winrt::hstring const& message) {
     const auto path = std::filesystem::temp_directory_path() / L"lithe-winui-error.log";
     if (const auto file = CreateFileW(path.c_str(), GENERIC_WRITE, FILE_SHARE_READ, nullptr,
-                                      CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+                                      OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
         file != INVALID_HANDLE_VALUE) {
+        SetFilePointer(file, 0, nullptr, FILE_END);
         const auto bytes = static_cast<DWORD>(message.size() * sizeof(wchar_t));
         DWORD written = 0;
         WriteFile(file, message.c_str(), bytes, &written, nullptr);
+        constexpr wchar_t newline[] = L"\r\n";
+        WriteFile(file, newline, sizeof(newline) - sizeof(wchar_t), &written, nullptr);
         CloseHandle(file);
     }
 }
@@ -35,8 +38,11 @@ App::App() {
 
 void App::OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&) {
     try {
+        WriteStartupError(L"OnLaunched: creating window");
         window_ = make<MainWindow>();
+        WriteStartupError(L"OnLaunched: window created");
         window_.Activate();
+        WriteStartupError(L"OnLaunched: window activated");
     } catch (winrt::hresult_error const& error) {
         WriteStartupError(error.message());
         throw;
